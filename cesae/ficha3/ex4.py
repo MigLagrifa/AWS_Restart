984fc957ea575610b269b2e0beca2691ad0a0a36
Miguel Lagrifa <miguelagrifa@hotmail.com>
2023-10-17 16:47:30 +0100

    aula 17/10

---
+++ cesae/ficha3/ex4.py
@@ -0,0 +1,5 @@
+num=int(input("Insira um numero Inteiro"))
+
+while (num<20 and num>0):
+    num=num-1
+    print(num)
---
+++ cesae/ficha3/ex6.py
@@ -0,0 +1,10 @@
+num1=1
+soma=0
+while (num1<=100):
+    print(num1)
+    soma = soma + num1
+    num1=num1+1
+
+
+
+print("somatorio",soma)
